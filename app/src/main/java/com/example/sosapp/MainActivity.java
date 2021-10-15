c57f0ed54afb3a4090ef890372caebac3a36f207
Vishal Karthikeyan <vishalkarthikeyan17@gmail.com>
2021-10-15 23:30:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sosapp/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.sosapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button toRegBtn,LoginBtn;
+    EditText email,pw;
+    ProgressBar pb;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    FirebaseAuth fAuth;//fAuth
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toRegBtn=findViewById(R.id.LoginToRegisterBtn);
+        email=findViewById(R.id.RegisterEmail);
+        pw=findViewById(R.id.RegisterPassword);
+        pb=findViewById(R.id.progressBarLogin);
+        LoginBtn=findViewById(R.id.LoginBtn);
+
+        database= FirebaseDatabase.getInstance();
+        myRef=database.getReference();
+        fAuth= FirebaseAuth.getInstance();
+
+        toRegBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Registration.class));
+            }
+        });
+
+        LoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String em=email.getText().toString().trim();
+                String pass=pw.getText().toString().trim();
+                if(TextUtils.isEmpty(em)){
+                    email.setError("Email is Required");
+                    return;
+                }
+                if(TextUtils.isEmpty(pass)){
+                    pw.setError("Password is Required");
+                    return;
+                }
+                if(pass.length()<6){
+                    pw.setError("Password Must be >= 6 Characters");
+                    return;
+                }
+                pb.setVisibility(View.VISIBLE);
+
+                fAuth.signInWithEmailAndPassword(em,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this,"Login Sucessfull",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this,"Error "+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                            pb.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                });
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/sosapp/Registration.java
@@ -0,0 +1,85 @@
+package com.example.sosapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Registration extends AppCompatActivity {
+
+    EditText fname,Remail,Rpw;
+    Button regBtn;
+    ProgressBar Rpb;
+    FirebaseAuth fAuth;
+    DatabaseReference reff;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        fname=findViewById(R.id.RegisterFullName);
+        Remail=findViewById(R.id.RegisterEmail);
+        Rpw=findViewById(R.id.RegisterPassword);
+        regBtn=findViewById(R.id.LoginBtn);
+        Rpb=findViewById(R.id.progressBarReg);
+
+        fAuth=FirebaseAuth.getInstance();
+        reff= FirebaseDatabase.getInstance().getReference();
+
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String em=Remail.getText().toString().trim();
+                String pass=Rpw.getText().toString().trim();
+                String namee=fname.getText().toString().trim();
+                if(TextUtils.isEmpty(em)){
+                    Remail.setError("Email is Required");
+                    return;
+                }
+                if(TextUtils.isEmpty(pass)){
+                    Rpw.setError("Password is Required");
+                    return;
+                }
+                if(pass.length()<6){
+                    Rpw.setError("Password Must be >= 6 Characters");
+                    return;
+                }
+                Rpb.setVisibility(View.VISIBLE);
+
+                fAuth.createUserWithEmailAndPassword(em,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            reff.child("Users").child(fAuth.getUid()).child("Name").setValue(namee);
+                            reff.child("Users").child(fAuth.getUid()).child("Email").setValue(em);
+                            Toast.makeText(Registration.this,"Registered",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }
+                        else{
+                            Toast.makeText(Registration.this,"Error "+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                            Rpb.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                });
+            }
+        });
+
+
+    }
+}
