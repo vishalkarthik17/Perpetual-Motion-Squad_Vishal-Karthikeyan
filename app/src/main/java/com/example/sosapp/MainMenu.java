f7de430fa8804bcf63767baac6edcb1dac683b50
Vishal Karthikeyan <vishalkarthikeyan17@gmail.com>
2021-10-20 11:35:56 +0530

    Updated MainMenu.java

--- app/src/main/java/com/example/sosapp/MainMenu.java
+++ app/src/main/java/com/example/sosapp/MainMenu.java
@@ -1,15 +1,22 @@
 package com.example.sosapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -53,9 +60,35 @@ public class MainMenu extends AppCompatActivity {
             }
         });
         LiveTracingBtn.setOnClickListener(new View.OnClickListener() {
+            boolean trouble=false;
+            String who="";
             @Override
             public void onClick(View v) {
-                //startActivity(new Intent(getApplicationContext(),MapsActivity.class));
+
+                reff.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for(DataSnapshot i : snapshot.child("Users").child(fAuth.getUid()).child("YourResp").getChildren()){
+                            String val= (String) i.getValue();
+                            if(val.equals("true")){
+                                boolean trouble=true;
+                                Intent liveScreen=new Intent(getApplicationContext(),MapsActivity.class);
+                                liveScreen.putExtra("uid",i.getKey().toString());
+                                startActivity(liveScreen);
+                            }
+                        }
+                        if(trouble==false){
+                            Toast.makeText(MainMenu.this, "No one in Trouble", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+
             }
         });
 
@@ -64,6 +97,20 @@ public class MainMenu extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 reff.child("Users").child(fAuth.getUid()).child("CanTrackMe").setValue("true");
+                reff.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for(DataSnapshot i : snapshot.child("Users").child(fAuth.getUid()).child("EmergencyContacts").getChildren()){
+                            String ec=i.getKey();
+                            reff.child("Users").child(ec).child("YourResp").child(fAuth.getUid()).setValue("true");
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
                 startService(new Intent(MainMenu.this,TrackingService.class));
             }
         });
@@ -73,6 +120,21 @@ public class MainMenu extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 reff.child("Users").child(fAuth.getUid()).child("CanTrackMe").setValue("false");
+                reff.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for(DataSnapshot i : snapshot.child("Users").child(fAuth.getUid()).child("EmergencyContacts").getChildren()){
+                            String ec=i.getKey();
+                            reff.child("Users").child(ec).child("YourResp").child(fAuth.getUid()).setValue("false");
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
                 stopService(new Intent(MainMenu.this,TrackingService.class));
             }
         });
